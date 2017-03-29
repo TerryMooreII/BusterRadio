6eebc79a931d14df6c6b2576e6d2fa9d968cd9a7
Terry Moore II <terry.moore.ii@gmail.com>
2017-03-29 19:05:09 -0400

    Count and badge

--- src/app/side-nav-menu/side-nav-menu.component.ts
+++ src/app/side-nav-menu/side-nav-menu.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from "@angular/core";
-import {ArchiveService} from "../services/archive/archive.service";
-import {CacheService} from "../services/cache/cache.service";
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ArchiveService} from '../services/archive/archive.service';
+import {CacheService} from '../services/cache/cache.service';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Component({
     selector: 'br-side-nav-menu',
@@ -10,27 +11,33 @@ import {Router} from "@angular/router";
 })
 export class SideNavMenuComponent implements OnInit {
 
+    totalArtists: number;
+
     constructor(private archiveService: ArchiveService, private cache: CacheService, private router: Router) {
     }
 
     ngOnInit() {
+         this.archiveService.getTotalArtists().subscribe(response => {
+             this.totalArtists = response._body.response.numFound;
+         });
+
     }
 
     randomShow() {
-        let artists = this.cache.getArtists();
+        const artists = this.cache.getArtists();
 
-        let artist = artists[Math.floor(Math.random() * artists.length)];
+        const artist = artists[Math.floor(Math.random() * artists.length)];
 
         this.archiveService.getShows(artist.identifier).subscribe((response) => {
-            let shows = response._body.response.docs;
+            const shows = response._body.response.docs;
             if (shows.length === 0) {
                 this.randomShow();
                 return;
             }
-            let show = shows[Math.floor(Math.random() * shows.length)];
+            const show = shows[Math.floor(Math.random() * shows.length)];
             this.router.navigate(['/artists', artist.identifier, 'years', show.year, 'shows', show.identifier]);
 
-        })
+        });
     }
 
 
